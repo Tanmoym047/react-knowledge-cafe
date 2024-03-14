8aeb4ac05dd7da76eac23d5aeee9c9fb9028be96
Tanmoym047 <tanmoym047@gmail.com>
2024-03-15 02:06:27 +0600

    time and bookmark added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
 
+import { useState } from 'react'
 import './App.css'
 import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
 import Header from './components/Header/Header'
 
 function App() {
+  const [bookmarks, setbookmarks] = useState([]);
+  const [readingTime, setReadingTime] = useState(0);
+
+  const handleAddToBookmark = blog => {
+    const newBookmarks = [...bookmarks, blog];
+    setbookmarks(newBookmarks);
+    // console.log(blog);
+  }
+  const handleMarkAsRead = time => {
+    // console.log('marking read', time);
+    const newReadingTime = readingTime + time;
+    setReadingTime(newReadingTime);
+  }
 
   return (
     <>
-      
+
       <Header></Header>
-      <Blogs></Blogs>
-      
+      <div className='md:flex max-w-7xl mx-auto'>
+        <Blogs handleAddToBookmark={handleAddToBookmark} handleMarkAsRead={handleMarkAsRead}></Blogs>
+        <Bookmarks bookmarks={bookmarks} readingTime={readingTime}></Bookmarks>
+      </div>
+
+
     </>
   )
 }
